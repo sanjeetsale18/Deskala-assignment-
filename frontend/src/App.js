a59033aae7d8ff5677f36d086e92ed1f6dce4fd3
sanjeetsale <salesanjeet8@gmail.com>
2022-06-17 08:27:41 +0530

    frontend setup and packages

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route></Route>
+      <Route></Route>
+    </Routes>
+  );
+}
+
+export default App;
